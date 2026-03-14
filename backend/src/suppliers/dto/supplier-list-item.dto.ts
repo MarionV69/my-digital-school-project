afb90c901c2e3b270d8908dde9e9bd922b405871
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-03-14 19:28:44 +0100

    feat: filterDto, ListItemDto & findAll() (#18)

---
+++ backend/src/suppliers/dto/supplier-list-item.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { PriceRange } from "../enums/price-range.enum";
+
+
+export class ListItemDto {
+
+    @ApiProperty({ example: 1 })
+    id: number;
+
+    @ApiProperty({ example: 'MAG '})
+    name: string;
+
+    @ApiProperty({ example: 'Lyon '})
+    city: string;
+
+    @ApiProperty({ example: 'ECONOMIC', enum: PriceRange})
+    priceRange: PriceRange;
+
+    @ApiPropertyOptional({ example: ['Bio', 'AOP'] })
+    labels?: string[];
+    
+    @ApiPropertyOptional({ example: ['Viandes', 'Poissons'] })
+    productCategories?: string[];
+
+}
--- backend/src/suppliers/suppliers.service.ts
+++ backend/src/suppliers/suppliers.service.ts
@@ -8,6 +8,8 @@ import { ProductCategory } from './entities/product-category.entity';
 import { Label } from './entities/label.entity';
 import { LabelDto } from './dto/label.dto';
 import { CategoryDto } from './dto/category.dto';
+import { FilterDto } from './dto/supplier-filter.dto';
+import { ListItemDto } from './dto/supplier-list-item.dto';
 
 @Injectable()
 export class SuppliersService {
@@ -30,8 +32,47 @@ export class SuppliersService {
     return `This action adds a new supplier ${JSON.stringify(createSupplierDto)}`;
   }
 
-  findAll() {
-    return `This action returns all suppliers`;
+  // Récupérer tous les fournisseurs (avec ou sans filtres)
+  async findAll(filters: FilterDto): Promise<ListItemDto[]> {
+
+    const query = this.supplierRepository
+      .createQueryBuilder('supplier')
+      .leftJoinAndSelect('supplier.labels', 'label')
+      .leftJoinAndSelect('supplier.productCategories', 'category')
+      .leftJoinAndSelect('supplier.supplier', 'establishment')
+
+    // Filtres conditionnels
+    if (filters.city) {
+      query.andWhere('supplier.city = :city', {city: filters.city});
+    }
+
+    if (filters.search) {
+      query.andWhere('supplier.name LIKE :search', { search: `%${filters.search}%` });
+    }
+
+    if (filters.priceRange) {
+      query.andWhere('supplier.priceRange = :priceRange', { priceRange: filters.priceRange});
+    }
+
+    if (filters.labels && filters.labels.length > 0) {
+      query.andWhere('label.name IN (:...labels)', { labels: filters.labels});
+    }
+
+    if (filters.productCategories && filters.productCategories.length > 0) {
+      query.andWhere('category.name IN (:...productCategories)', { productCategories: filters.productCategories});
+    }
+  
+    // Transformation des entités en ListItemDto
+    const suppliers = await query.getMany();
+
+    return suppliers.map(supplier => ({
+      id: supplier.supplierId,
+      name: supplier.supplier.legalName,
+      city: supplier.supplier.city,
+      priceRange: supplier.priceRange,
+      labels: supplier.labels.map(label => label.name),
+      productCategories: supplier.productCategories.map(ProductCategory => ProductCategory.name),
+    }))
   }
 
   findOne(id: number) {
