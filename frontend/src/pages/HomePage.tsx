80f465e7a2e6e78bba38acbfee49bca58c1e2d1c
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-04-18 16:54:53 +0200

    feat: add SearchBar (#71)

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,20 @@
+import SearchBar from "@/components/home/SearchBar";
+import axios from "axios";
 import { User } from "lucide-react";
 import { Link } from "react-router-dom";
 
 function HomePage() {
+
+  function handleSearch(query: string, location: string){
+    const params : Record<string, string> = {};
+    if (query) params.query = query;
+    if (location) params.location = location;
+
+    // GET /suppliers?query=boucherie&location=Lyon
+    axios.get("/suppliers", {params});
+  }
+
+
   return (
     <div>
       <h1>HomePage</h1>
@@ -9,6 +22,9 @@ function HomePage() {
         <User />
         Se connecter
       </Link>
+      <SearchBar onSearch={handleSearch}>
+
+      </SearchBar>
     </div>
   );
 }
