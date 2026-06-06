08ae24eba14079f68f30ce72d497dd9de92e158c
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-06-06 16:58:12 +0200

    feat: add logo in SupplierDetails (102

--- frontend/src/components/supplier-details/CoverPhoto.tsx
+++ frontend/src/components/supplier-details/CoverPhoto.tsx
@@ -6,12 +6,21 @@ type CoverPhotoProps = {
 
 export default function CoverPhoto({supplier}: CoverPhotoProps) {
     return(
-        <div className="w-full h-48 lg:h-96 relative">
-          <img
-            src={supplier.coverPhotoUrl}
-            alt={supplier.name}
-            className="w-full h-full object-cover"
-          />
+        <div className="relative w-full h-48 lg:h-96">
+            <img
+                src={supplier.coverPhotoUrl}
+                alt={supplier.name}
+                className="w-full h-full object-cover"
+            />
+            {supplier.logoUrl && (
+                <div className="absolute -bottom-8 left-6 lg:left-24 p-4 rounded-lg border-white shadow-md bg-white">
+                    <img
+                        src={supplier.logoUrl}
+                        alt={`Logo ${supplier.name}`}
+                        className="w-auto h-20 lg:h-24 object-cover"
+                    />
+                </div>
+            )}
         </div>
     )
 }
